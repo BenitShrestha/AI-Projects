67c7ac0390f89f73e079e693749ab2faa9e7cea8
BenitShrestha <quinjet01234@gmail.com>
2024-07-07 17:53:55 +0545

    Update reference images using Dictionary, and conditions

--- Live_Face_Recognition/main.py
+++ Live_Face_Recognition/main.py
@@ -18,25 +18,25 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 """ Model checks camera frame based on counter, in fps """
 counter = 0 
-
 face_match = False
 flag = 0 
 
 # Load reference image
-reference_img = cv2.imread('Live_Face_Recognition/Images/reference_img_six.png')
-second_reference_img = cv2.imread('Live_Face_Recognition/Images/reference_img_basanta.jpg')
+reference_images = {
+    1: cv2.imread('Live_Face_Recognition/Images/reference_img_six.png'),
+    2: cv2.imread('Live_Face_Recognition/Images/reference_img_basanta.jpg'),
+    3: cv2.imread('Live_Face_Recognition/Images/reference_img_panday.jpg')
+}
 
 def check_face(frame): # Machine learning function to check if user is Benit
     global face_match
     global flag
     try: # Verify using DeepFace, copy() to avoid locking scenarios
-        if DeepFace.verify(frame, reference_img.copy())['verified']:
-            face_match = True
-            flag = 1
-        elif DeepFace.verify(frame, second_reference_img.copy())['verified']:
-            face_match = True
-            flag = 2
-        else:
+        for id, ref in reference_images.items():
+            if DeepFace.verify(framne, ref_img.copy())['verified']:
+                face_match = True
+                flag = id 
+                return
             face_match = False
     except ValueError:
         face_match = False
@@ -49,18 +49,21 @@ while True:
     ret, frame = cap.read()
 
     if ret:
-        if counter % 30 == 0: # Run every 30 frames
+        if counter % 15 == 0: # Run every 30 frames
             try: # Starting a thread
                 threading.Thread(target = check_face, args = (frame.copy(),)).start() # Comma put because args needs to be a tuple 
             except ValueError:
                 pass
         counter += 1 # Increment counter
 
-        if face_match and flag == 1:
+        if face_match:
             """ If face is matched, display text on frame, font scale, color: BGR, thickness """
-            cv2.putText(frame, "Benit no glasses!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 2)
-        elif face_match and flag == 2:
-            cv2.putText(frame, "Basanta!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 2)
+            if flag == 1:
+                cv2.putText(frame, "Benit no glasses!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 255, 0), 2)
+            elif flag == 2:
+                cv2.putText(frame, "Basanta!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 2)
+            elif flag == 3:
+                cv2.putText(frame, "Panday!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
         else:
             cv2.putText(frame, 'Not Recognized!', (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 0 , 255), 2)
 
