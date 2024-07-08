420b85da4b3f3c6f40e0d7a6ebbb49c79feee023
BenitShrestha <quinjet01234@gmail.com>
2024-07-08 20:44:11 +0545

    Added list of model labels, so they can be changed easily

--- Poetic_Text_Generation/run.py
+++ Poetic_Text_Generation/run.py
@@ -6,9 +6,11 @@ import random
 from main import load_data, sample, generate_text
 import torch
 
+name = ['base', 'base_droput', 'BiLSTM']
 def main():
     text, char_to_index, index_to_char, characters = load_data()
-    model = tf.keras.models.load_model('Models/poetic_text_generator_base.keras')
+    """ Names of saved models: [base, base_droput, BiLSTM] -- Change index as per need """
+    model = tf.keras.models.load_model(f'Models/poetic_text_generator_{name[0]}.keras')
 
     # Get user input for the length and temperature
     length = int(input("Enter the length of text to generate: "))
