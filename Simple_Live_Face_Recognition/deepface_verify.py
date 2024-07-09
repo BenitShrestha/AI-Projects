fffe25122aad7bdc4a228931d091df328e4e817a
BenitShrestha <quinjet01234@gmail.com>
2024-07-09 19:14:41 +0545

    Changes made to image labels, main.py stored for version control later

--- Simple_Live_Face_Recognition/deepface_verify.py
+++ Simple_Live_Face_Recognition/deepface_verify.py
@@ -19,7 +19,7 @@ def preprocess_image(img_path):
     return img
 
 # Load reference image
-reference_img_path = r'Simple_Live_Face_Recognition\Images\benitglasses_one.jpg'
+reference_img_path = r'Simple_Live_Face_Recognition\Images\ref_benit_phone.jpg'
 try:
     reference_img = preprocess_image(reference_img_path)
 except FileNotFoundError as e:
@@ -27,7 +27,7 @@ except FileNotFoundError as e:
     exit(1)
 
 # Load test image
-test_img_path = r'Simple_Live_Face_Recognition\Images\benitglasses_compare.jpg'
+test_img_path = r'Simple_Live_Face_Recognition\Images\reference_img_basanta.jpg'
 try:
     test_img = preprocess_image(test_img_path)
 except FileNotFoundError as e:
--- Simple_Live_Face_Recognition/main.py
+++ Simple_Live_Face_Recognition/main.py
@@ -13,27 +13,22 @@ from deepface import DeepFace
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 # Set width and height of camera frame
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 """ Model checks camera frame based on counter, in fps """
 counter = 0 
 face_match = False
 flag = 0 
-# debounce_counter = 0
-# debounce_threshold = 3
 
 # Load reference image
 reference_images = {
-    1: cv2.imread('Simple_Live_Face_Recognition/Images/reference_img_three.jpg'),
-    2: cv2.imread('Simple_Live_Face_Recognition/Images/reference_img_basanta.jpg'),
-    3: cv2.imread('Simple_Live_Face_Recognition/Images/reference_img_panday.jpg'),
-    4: cv2.imread('Simple_Live_Face_Recognition/Images/benitglasses_compare.jpg')
+    1: 'Simple_Live_Face_Recognition/Images/benit_phone_NG_1.jpg',
+    2: 'Simple_Live_Face_Recognition/Images/benit_phone_G_1.jpg',
+    3: 'Simple_Live_Face_Recognition/Images/basanta_webcam_1.jpg',
+    4: 'Simple_Live_Face_Recognition/Images/hasana_webcam_1.jpg',
+    5: 'Simple_Live_Face_Recognition/Images/ashish_webcam_1.jpg'
 }
-
-# Ensure image is loaded properly 
-for ref in reference_images.values():
-    assert ref is not None, f"{ref} could not be loaded"
 
 def preprocess_image(image, target_size = (224, 224)):
     """ Preprocess image by Resizing, normalizing pixel values, converting color spaces and histogram equalization """
@@ -54,25 +49,30 @@ def preprocess_image(image, target_size = (224, 224)):
     return image_equalized
 
 for idx in reference_images:
-    reference_images[idx] = preprocess_image(reference_images[idx])
+    reference_images[idx] = preprocess_image(cv2.imread(reference_images[idx]))
+    cv2.imwrite(f'Simple_Live_Face_Recognition/Preprocessed_Images/reference_img_{idx}.jpg', reference_images[idx])
+
 
 def check_face(frame): # Machine learning function to check if user is Benit
     global face_match
     global flag
-    # global debounce_counter
     try: # Verify using DeepFace, copy() to avoid locking scenarios
-        for idx, ref in reference_images.items():
+        for idx, ref_path in reference_images.items():
+            if not os.path.exists(ref_path):
+                print(f'Path does not exist: {ref_path}')
+                continue
+            ref = cv2.imread(ref_path)
+            if ref is None:
+                print(f'Error loading image from path: {ref_path}')
+                continue
             result = DeepFace.verify(frame, ref.copy())
-            if result.get('verified', False): # and result.get('confidence', 0.0) >= 0.9:
-                # debounce_counter += 1
-                # if debounce_counter >= debounce_threshold:
+            if result['verified']: # and result.get('confidence', 0.0) >= 0.9: # Confidence threshold
                 face_match = True # Indent Back
                 flag = idx
-                # debounce_counter = 0
+                print(f'Face: {ref_path}, Flag: {flag}')
                 return
-                # else:
-                #     debounce_counter = 0
-            face_match = False
+            else:
+                face_match = False
     except (ValueError, KeyError) as e:
         face_match = False
         # print(f"Error while verifying face: {e}")
@@ -85,35 +85,38 @@ while True:
     ret, frame = cap.read()
 
     if ret:
-
-        # Convert BGR frame to RGB format 
-        # frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
         # Preprocess captured frame
         preprocessed_frame = preprocess_image(frame)
 
-        # Save the frame as an image 
-        cv2.imwrite(f'Simple_Live_Face_Recognition/Images/captured_frame_01.jpg', preprocessed_frame)
+        # # Save the frame as an image 
+        # cv2.imwrite(f'Simple_Live_Face_Recognition/Images/captured_frame_01.jpg', preprocessed_frame)
+        cv2.imwrite(f'Simple_Live_Face_Recognition/Images/captured_frame_01.jpg', frame)
 
-        if counter % 30 == 0: # Run every 30 frames
+        if counter % 15 == 0: # Run every 30 frames
             try: # Starting a thread
-                threading.Thread(target = check_face, args = (preprocessed_frame.copy(),)).start() # Comma put because args needs to be a tuple 
+                threading.Thread(target = check_face, args = (preprocessed_frame.copy(),)).start() # Preprocessing
+                # threading.Thread(target = check_face, args = (frame.copy(),)).start() # Comma put because args needs to be a tuple 
+
             except ValueError:
                 pass
         counter += 1 # Increment counter
 
         """ If face is matched, display text on frame, font scale, color: BGR, thickness """
-        if flag == 1 and face_match:
-            cv2.putText(frame, "Benit No Glasses!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 2)
+        if face_match:
+            if flag == 1:
+                cv2.putText(frame, "Benit with Glasses!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 2)
 
-        # elif flag == 2 and face_match:
-        #     cv2.putText(frame, "Basanta!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 255, 0), 2)
+            elif flag == 2:
+                cv2.putText(frame, "Benit without Glasses!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 2)
 
-        elif flag == 3 and face_match:
-            cv2.putText(frame, "Panday!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
+            elif flag == 3:
+                cv2.putText(frame, "Basanta!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
 
-        elif flag == 4 and face_match:
-            cv2.putText(frame, "Benit with glasses!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
+            elif flag == 4:
+                cv2.putText(frame, "Hasana!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
+
+            elif flag == 5:
+                cv2.putText(frame, "Ashish!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (255, 0, 0), 2)
 
         else:
             cv2.putText(frame, 'Not Recognized!', (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 0 , 255), 2)
