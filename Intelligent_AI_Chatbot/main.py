b52ae668202471dff97b1e50622cd15d7dbaaf0a
BenitShrestha <quinjet01234@gmail.com>
2024-07-10 21:40:34 +0545

    Clean up sentence & Bag of Words functions added

--- Intelligent_AI_Chatbot/main.py
+++ Intelligent_AI_Chatbot/main.py
@@ -15,9 +15,23 @@ lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('Intelligent_AI_Chatbot/intents.json').read())
 
 words = pickle.load(open('Intelligent_AI_Chatbot/Pickle_Files/words.pkl', 'rb'))
-clases = pickle.load(open('Intelligent_AI_Chatbot/Pickle_Files/classes.pkl', 'rb'))
+classes = pickle.load(open('Intelligent_AI_Chatbot/Pickle_Files/classes.pkl', 'rb'))
 model = load_model('Intelligent_AI_Chatbot/Models/Chat_Bot_Model.keras')
 
 # Working Functions
 def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence) # First tokenize
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words] # Second Lemmatize
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
     pass
