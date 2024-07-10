cdfc05f5c66e7004834081618bea10cef4112bff
BenitShrestha <quinjet01234@gmail.com>
2024-07-10 07:54:42 +0545

    Pickles files for words and classes for serialization

--- Intelligent_AI_Chatbot/training.py
+++ Intelligent_AI_Chatbot/training.py
@@ -32,9 +32,58 @@ for intent in intents['intents']: # Key intents - Basically a dictionary
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-print(words) # Includes repeated as well as unique words
+# print(words) # Includes repeated as well as unique words
 # print(documents)
 
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+
 words = sorted(set(words))
-print(words) # Lemmatized words, unique and sorted list
+classes = sorted(set(classes))
+
+# print(words) # Lemmatized words, unique and sorted list
+# print(classes) # Unique tags
+
+pickle.dump(words, open('Intelligent_AI_Chatbot/Pickle Files/words.pkl', 'wb'))
+pickle.dump(classes, open('Intelligent_AI_Chatbot/Pickle Files/classes.pkl', 'wb'))
+
+""" Bag of Words - 0s and 1s: If word is present, 1, else 0 """
+training = []
+output_empty = [0] * len(classes) # List of zeros with length of classes
+
+# # Preparing training data
+# for document in documents:
+#     bag = []
+#     word_patterns = document[0]
+#     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+#     for word in words:
+#         bag.append(1) if word in word_patterns else bag.append(0)
+
+#     output_row = list(output_empty)
+#     output_row[classes.index(document[1])] = 1
+#     training.append([bag, output_row])
+ 
+# # Shuffle, randomize data
+# random.shuffle(training)
+# training = np.array(training)
+
+# train_x = list(training[:, 0])
+# train_y = list(training[:, 1])
+
+# # Building Model
+# model = Sequential()
+# model.add(Dense(128, input_shape = (len(train_x[0]), ) , activation = 'relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(64, activation = 'relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(len(train_y[0]), activation = 'softmax'))
+
+# # Optimizer, loss function and compilation
+# sgd = SGD(lr = 0.01, decay = 1e-6, momentum = 0.9, nesterov = True)
+# model.compile(loss = 'categorical_crossentropy', optimizer = sgd, metrics = ['accuracy'])
+
+# # Fitting
+# model.fit(np.array(train_x), np.array(train_y), epochs = 200, batch_size = 5, verbose = 1)
+
+# model.save('Intelligent_AI_Chatbot/Models/Chat_Bot_Model.keras')
+
+# print("Done!")
