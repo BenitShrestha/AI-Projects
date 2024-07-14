ac2865742785a13cd2a42575bebe5d6a871168f0
BenitShrestha <quinjet01234@gmail.com>
2024-07-14 12:15:02 +0545

    Documentation added to code
    Done for future reference, elaborating functionaltiy of code as well

--- Simple_Live_Face_Recognition/main.py
+++ Simple_Live_Face_Recognition/main.py
@@ -1,3 +1,4 @@
+# Importing necessary Libraries and Modules
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import threading 
@@ -21,10 +22,13 @@ reference_images = {
 # Image preprocessing - recolor, resize, normalize
 def preprocess_image(image, target_size=(256, 256)):
     """ Preprocess captured frames """
+    # Converts BGR to RGB
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+    # Resizes image to 256x256
     image_resized = cv2.resize(image_rgb, target_size)
 
+    # Normalizes image - values between 0 and 1
     image_normalized = image_resized / 255.0
 
     # Apply histogram equalization
@@ -38,9 +42,9 @@ def preprocess_image(image, target_size=(256, 256)):
 
 # Preprocess reference images
 for idx, ref_path in reference_images.items():
-    ref_image = cv2.imread(ref_path)
+    ref_image = cv2.imread(ref_path) # Accept image path to be read
 
-    if not os.path.exists(ref_path):
+    if not os.path.exists(ref_path): # Check if file exists
         print(f"Error: File not found - {ref_path}")
         continue
 
@@ -49,9 +53,10 @@ for idx, ref_path in reference_images.items():
         continue
 
     else:
-        img_name = ref_path[36:-4]
-        print(img_name)
+        img_name = ref_path[36:-4] # Slicing to get image name/label
+        # print(img_name)
         preprocessed_image = preprocess_image(ref_image)
+        # Rename the image as preprocessed one, for referencing later
         cv2.imwrite(f'Simple_Live_Face_Recognition/Preprocessed_Images/{img_name}_preprocessed.jpg', preprocessed_image)
 
         reference_images[idx] = ref_image # Loads dictonary with cv2.imread(paths)
@@ -61,6 +66,7 @@ def check_face(frame):
     global face_match, flag
     try:
         face_match = False
+        # Iterate through reference images along with the indices
         for idx, ref_image in reference_images.items():
             result = DeepFace.verify(frame, ref_image.copy())  # Verify face with each reference image
             if result['verified']:
@@ -76,17 +82,20 @@ def check_face(frame):
 counter = 0
 while True:
     ret, frame = cap.read()  # Capture frame from camera
-    if ret:
+    if ret: # Indication that image was captured
         preprocessed_frame = preprocess_image(frame)  # Preprocess captured frame
         if counter % 15 == 0:  # Run face verification every 15 frames
             try:
-                threading.Thread(target=check_face, args=(preprocessed_frame.copy(),)).start()  # Start face verification in a thread
+                # Start face verification in a thread so that operations can run concurrently with the main thread
+                # Main thread - Handles capturing frames, updating UI, while face verification executes independently
+                threading.Thread(target=check_face, args=(preprocessed_frame.copy(),)).start()  
             except ValueError:
                 pass
-        counter += 1
+        counter += 1 # Raise counter by 1, Frame counting
 
         # Display recognition result on the frame
         if face_match:
+            ''' Check for individual condtitions of reference images used '''
             if flag == 1:
                 cv2.putText(frame, "Benit with Glasses!", (20, 450), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 0), 2)
             elif flag == 2:
@@ -103,8 +112,8 @@ while True:
         cv2.imshow('Live Face Recognition', frame)  # Display frame with face recognition overlay
 
     key = cv2.waitKey(1)
-    if key == ord("q"):
+    if key == ord("q"): # Quit if 'q' key is pressed
         break
 cv2.imwrite(f'Simple_Live_Face_Recognition/Images/1_captured_frame.jpg', preprocessed_frame)  # Save captured frame
 cv2.destroyAllWindows()
-cap.release()
+cap.release() # Release resources related to cap
